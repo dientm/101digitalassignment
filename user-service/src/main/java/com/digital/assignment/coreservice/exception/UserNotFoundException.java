b79015f78488acd4311d93021df4f736ef321775
dtran <dientran@fortna.com>
2021-11-01 16:20:54 +0700

    clean

--- user-service/src/main/java/com/digital/assignment/coreservice/exception/UserNotFoundException.java
+++ user-service/src/main/java/com/digital/assignment/coreservice/exception/UserNotFoundException.java
@@ -2,9 +2,6 @@ package com.digital.assignment.coreservice.exception;
 
 import javassist.NotFoundException;
 
-/**
- * @author dtran@fortna.com
- */
 public class UserNotFoundException extends NotFoundException {
     public UserNotFoundException(Long id) {
         super("Could not find user:  " + id);
